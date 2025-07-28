077a0e784a74b69ca4aa6b237f89fbd66ae71ff6
llgrl <129662281+llgrl@users.noreply.github.com>
2025-07-28 17:38:21 +0800

    Update manager.py

--- CEACStatusBot/notification/manager.py
+++ CEACStatusBot/notification/manager.py
@@ -19,19 +19,15 @@ class NotificationManager():
         self.__handleList.append(notificationHandle)
 
     def send(self) -> None:
-        res = query_status(self.__location, self.__number, self.__passport_number, self.__surname, self.__captchaHandle)
-        current_status = res['status']
+    res = query_status(self.__location, self.__number, self.__passport_number, self.__surname, self.__captchaHandle)
+    current_status = res['status']
 
-        # Load the previous statuses from the file
-        statuses = self.__load_statuses()
+    statuses = self.__load_statuses()
 
-        # Check if the current status is different from the last recorded status
-        if not statuses or current_status != statuses[-1]['status']:
-            self.__save_current_status(current_status)
-            self.__send_notifications(res)
-        else:
-            print("Status unchanged. No notification sent.")
+    if not statuses or current_status != statuses[-1]['status']:
+        self.__save_current_status(current_status)
 
+    self.__send_notifications(res)
     def __load_statuses(self) -> list:
         if os.path.exists(self.__status_file):
             with open(self.__status_file, 'r') as file:
